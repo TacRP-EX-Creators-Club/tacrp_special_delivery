e20546d65199b9eb759ba180056a764f0163de57
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-06-10 12:30:02 -0400

    gangster update

---
+++ lua/weapons/tacrp_sd_dual_degala.lua
@@ -0,0 +1,300 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Dual Eagles"
+SWEP.Category = "Tactical RP" // "Tactical RP (Arctic)"
+
+SWEP.SubCatTier = "1Elite"
+SWEP.SubCatType = "1Sidearm"
+
+SWEP.Description = "As if the Deagle wasn't already impractical enough, you've channeled your inner gangster and decieded to dual-wield a pair of them sporting cheap gold finishes."
+
+SWEP.ViewModel = "models/weapons/tacint_shark/v_dual_eagles.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_mtx.mdl"
+
+SWEP.Slot = 1
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+        Damage_Max = 24,
+        Damage_Min = 12,
+        RPM = 750,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 3.75,
+            [HITGROUP_CHEST] = 1,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+
+        Spread = 0.02,
+
+        MoveSpeedMult = 0.95,
+        ShootingSpeedMult = 0.8,
+        SightedSpeedMult = 1,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+    [TacRP.BALANCE_TTT] = {
+        Damage_Max = 14,
+        Damage_Min = 6,
+        Range_Min = 500,
+        Range_Max = 1500,
+        RPM = 600,
+
+        Spread = 0.02,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 2.5,
+            [HITGROUP_CHEST] = 1,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+
+        ReloadTimeMult = 1.25,
+
+        MoveSpeedMult = 1,
+        ShootingSpeedMult = 0.9,
+        MeleeSpeedMult = 1,
+        SightedSpeedMult = 0.75,
+        ReloadSpeedMult = 0.9,
+    },
+    [TacRP.BALANCE_PVE] = {
+        Damage_Max = 18,
+        Damage_Min = 9,
+        RPM = 750,
+
+        Spread = 0.01,
+
+        MoveSpeedMult = 0.95,
+        ShootingSpeedMult = 0.8,
+        SightedSpeedMult = 1,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+}
+
+SWEP.TTTReplace = {["weapon_zm_pistol"] = 0.5, ["weapon_ttt_glock"] = 0.5}
+
+// "ballistics"
+
+SWEP.Damage_Max = 28
+SWEP.Damage_Min = 15
+SWEP.Range_Min = 1000 // distance for which to maintain maximum damage
+SWEP.Range_Max = 3000 // distance at which we drop to minimum damage
+SWEP.Penetration = 1 // units of metal this weapon can penetrate
+SWEP.ArmorPenetration = 0.6
+
+SWEP.MuzzleVelocity = 9500
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 4.5,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 0.9,
+    [HITGROUP_RIGHTARM] = 0.9,
+    [HITGROUP_LEFTLEG] = 0.75,
+    [HITGROUP_RIGHTLEG] = 0.75,
+    [HITGROUP_GEAR] = 0.9
+}
+
+// misc. shooting
+
+SWEP.Firemodes = {-2}
+SWEP.Firemode = -2
+SWEP.RunawayBurst = true
+SWEP.PostBurstDelay = 0.07
+
+SWEP.RPM = 500
+
+SWEP.Spread = 0.015
+
+SWEP.MoveSpreadPenalty = 0
+SWEP.MidAirSpreadPenalty = 0.075
+SWEP.HipFireSpreadPenalty = 0
+
+SWEP.RecoilPerShot = 2
+SWEP.RecoilMaximum = 20
+SWEP.RecoilResetTime = 0.15
+SWEP.RecoilDissipationRate = 20
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 5
+
+SWEP.RecoilKick = 10
+
+SWEP.RecoilSpreadPenalty = 0.002
+
+SWEP.Sway = 0.5
+
+SWEP.CanBlindFire = false
+SWEP.CanSuicide = true
+
+SWEP.ShootTimeMult = 0.6
+
+// handling
+
+SWEP.MoveSpeedMult = 0.975
+SWEP.ShootingSpeedMult = 0.9
+SWEP.SightedSpeedMult = 1
+
+SWEP.ReloadSpeedMult = 0.6
+
+SWEP.AimDownSightsTime = 0.25
+SWEP.SprintToFireTime = 0.25
+
+// hold types
+
+SWEP.HoldType = "duel"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeBlindFire = false
+SWEP.HoldTypeSuicide = "duel"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_DUEL
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_DUEL
+
+SWEP.PassiveAng = Angle(5, 0, -2)
+SWEP.PassivePos = Vector(3, 0, -6)
+
+SWEP.CustomizeAng = Angle(0, 35, 0)
+SWEP.CustomizePos = Vector(1, 0, -12)
+
+SWEP.BlindFireAng = Angle(0, 0, 0)
+SWEP.BlindFirePos = Vector(0, 0, 0)
+
+SWEP.BlindFireSuicideAng = Angle(0, 130, 0)
+SWEP.BlindFireSuicidePos = Vector(1, 22, -15)
+
+SWEP.SprintAng = Angle(0, 30, 0)
+SWEP.SprintPos = Vector(1, 0, -12)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_PISTOL
+SWEP.HolsterPos = Vector(0, 3, -4)
+SWEP.HolsterAng = Angle(60, 5, 0)
+
+// reload
+
+SWEP.ClipSize = 16
+SWEP.Ammo = "pistol"
+
+SWEP.ReloadTimeMult = 1
+
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/gsr1911.mdl"
+SWEP.DropMagazineImpact = "pistol"
+SWEP.DropMagazineAmount = 2
+
+SWEP.ReloadUpInTime = 2.8
+SWEP.DropMagazineTime = 0.25
+
+// sounds
+
+local path = "tacint_shark/weapons/dualeagles/"
+local path1 = "tacrp/weapons/mtx/"
+
+SWEP.Sound_Shoot = "^" .. path .. "elite-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 4
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "draw",
+    ["blind_idle"] = "idle",
+    ["blind_fire"] = "idle",
+    ["melee"] = {"melee1", "melee2"},
+    ["shoot_left"] = {"shoot_left-1", "shoot_left-2"},
+    ["shoot_right"] = {"shoot_right-1", "shoot_right-2"}
+}
+
+SWEP.LastShot = true
+SWEP.Akimbo = true
+
+// attachments
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_holster"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [4] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+    [2] = {
+        PrintName = "Trigger",
+        Category = {"trigger_akimbo"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [3] = {
+        PrintName = "Ammo",
+        Category = {"ammo_pistol"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    }
+}
+
+SWEP.AttachmentCapacity = 30 // amount of "Capacity" this gun can accept
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_dualeagles.clip_in_left", path .. "elite_leftclipin.wav")
+addsound("tacint_dualeagles.clip_in_right", path .. "elite_rightclipin.wav")
+addsound("tacint_dualeagles.clip_out", path .. "elite_clipout.wav")
+addsound("tacint_dualeagles.slide_back", path1 .. "slide_back.wav")
+addsound("tacint_dualeagles.slide_release", path1 .. "slide_release.wav")
+addsound("tacint_dualeagles.slide_shut", path .. "sliderelease.wav")
+
+SWEP.Scope = false
+
+SWEP.FreeAim = false
