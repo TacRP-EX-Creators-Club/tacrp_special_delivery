12a4e9df5dd20ef2205d6ce8e33fec1057603ecd
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-06-11 12:15:17 -0400

    remaining luas + workshop description

--- README.md
+++ README.md
@@ -6,7 +6,35 @@
 
 -------------------------------------------------
 
-Special Delivery is a small expansion pack for TacRP that includes weapons that required additional work in order to bring to life within Tactical Interventions flashy style.  Most of these required substantial animation edits or entirely custom animations in order to implement them, a factor that was particularly limiting in the creation of TacRP Interops, which sought to exclusively use TacInt's animations with minor edits.
+A small pack of 14 new weapons for TacRP, featuring more delightfully crusty Gamebanana models that some of you might recognize from classic GMod weapon addons.  Built around TacRP's mechanics and using modified TacInt animations, Special Delivery is the perfect addition to any tactical GMod player's arsenal.
 
-There are also some additional goodies in this pack, oddball stuff that you don't see often in FPS games, and maybe even a few fictional weapons.
- 
+[h1]But what's so special about this new pack?[/h1]
+
+TacRP InterOps was an expansion featuring a multitude of new weapons, all created by taking models from third-party sources and rigging them onto TI's animations.  This posed a unique challenge, as certain weapon types like bullpups and break-action weapons couldn't be implemented owing to TI having no weapon with a suitable set of animations for them.  While some minor animation edits were made, we strayed away from more ambitious modifications, owing to the large amount of (mostly unused) animations that would need to be edited for every minor change we made (nearly 70-per weapon!).
+
+That was until shortly after InterOps' completion when I felt the motivation to start seriously editing TacInt's animations in order to implement weapons we were previously unable to.  The weapon pack started with a loose theming of obscure and exotic weapons, but ultimately became somewhat of an InterOps addendum, touching on weapon types that weren't covered by that pack.
+
+This weapon pack aims to pick up the pieces of what was ultimately left out of InterOps, and serve as a fun, natural addition to the expanding TacRP arsenal.
+
+[h1]Features:[/h1]
+[*] Fourteen weapons with worldmodels, c_hands, and stylish animations from [i]Tactical Intervention[/i], edited by yours truly
+[*] Fine tuned balance with 4 different modes for different styles of play
+[*] Customize your weapons with a wide range of attachments. Make meaningful decisions and adjust your guns to your preference, and gain an advantage with unique tactical attachments like blind-fire cameras, blinding flashlights, and radars.
+[*] Melee bash close enemies, Blindfire above and around cover or quickthrow grenades, with animations for every weapon
+
+[h1]Weapon List[/h1]
+
+[*] CZ 52
+[*] AAC Honey Badger
+[*] FAMAS F1
+[*] PP-19 Bizon
+[*] Walther WA2000
+[*] PKM
+[*] MP 40
+[*] OTs-14 Groza
+[*] M1928 Thompson
+[*] "Duelling Dragons" Berettas
+[*] Dual USP Matches
+[*] Dual Desert Eagles
+[*] Combat Knife
+[*] SOG "Fasthawk" Tomahawk
---
+++ lua/weapons/tacrp_sd_groza.lua
@@ -0,0 +1,338 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "OTs-14-4 'Groza'"
+SWEP.Category = "Tactical RP" // "Tactical RP (Arctic)"
+
+SWEP.SubCatTier = "2Operator"
+SWEP.SubCatType = "4Assault Rifle"
+
+SWEP.Description = "Unique experimental rifle, essentially an AK reconfigured as a bullpup."
+
+SWEP.ViewModel = "models/weapons/tacint/v_ak47.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_ak47.mdl"
+
+SWEP.Slot = 2
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+        Damage_Max = 25,
+        Damage_Min = 15,
+
+        RecoilKick = 2,
+
+        MoveSpeedMult = 0.85,
+        ShootingSpeedMult = 0.75,
+        SightedSpeedMult = 0.75,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+    [TacRP.BALANCE_TTT] = {
+        Damage_Max = 16,
+        Damage_Min = 11,
+
+        Range_Min = 400,
+        Range_Max = 2000,
+
+        RPM = 600,
+
+        RecoilSpreadPenalty = 0.0025,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 2.5,
+            [HITGROUP_CHEST] = 1.25,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+
+        ReloadTimeMult = 1.25,
+
+        MoveSpeedMult = 0.9,
+        ShootingSpeedMult = 0.8,
+        MeleeSpeedMult = 1,
+        SightedSpeedMult = 0.55,
+        ReloadSpeedMult = 0.65,
+    },
+    [TacRP.BALANCE_PVE] = {
+        Damage_Max = 8,
+        Damage_Min = 4,
+
+        MoveSpeedMult = 0.85,
+        ShootingSpeedMult = 0.75,
+        SightedSpeedMult = 0.75,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+}
+
+SWEP.TTTReplace = TacRP.TTTReplacePreset.AssaultRifle
+
+// "ballistics"
+
+SWEP.Damage_Max = 20
+SWEP.Damage_Min = 14
+SWEP.Range_Min = 1200 // distance for which to maintain maximum damage
+SWEP.Range_Max = 3000 // distance at which we drop to minimum damage
+SWEP.Penetration = 7 // units of metal this weapon can penetrate
+SWEP.ArmorPenetration = 0.725
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 5,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1.25,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 0.85,
+    [HITGROUP_RIGHTLEG] = 0.85,
+    [HITGROUP_GEAR] = 0.9
+}
+
+SWEP.MuzzleVelocity = 25000
+
+// misc. shooting
+
+SWEP.Firemodes = {
+    2,
+    1
+}
+
+SWEP.RPM = 750
+
+SWEP.Spread = 0.003
+
+SWEP.ShootTimeMult = 0.5
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 12
+SWEP.RecoilResetTime = 0.2
+SWEP.RecoilDissipationRate = 20
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.75
+
+SWEP.RecoilKick = 4
+
+SWEP.RecoilSpreadPenalty = 0.002
+SWEP.HipFireSpreadPenalty = 0.03
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.925
+SWEP.ShootingSpeedMult = 0.85
+SWEP.SightedSpeedMult = 0.65
+
+SWEP.ReloadSpeedMult = 0.5
+
+SWEP.AimDownSightsTime = 0.4
+SWEP.SprintToFireTime = 0.4
+
+SWEP.Sway = 1.25
+SWEP.ScopedSway = 0.15
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -4)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(3, -2, -5)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+SWEP.SightAng = Angle(0.05, 0, 0)
+SWEP.SightPos = Vector(-4.66, -7.5, -2.9)
+
+SWEP.CorrectivePos = Vector(0, 0, -0.05)
+SWEP.CorrectiveAng = Angle(0.75, 0.7, 0)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_BACK
+SWEP.HolsterPos = Vector(5, -2, -6)
+SWEP.HolsterAng = Angle(0, 0, 0)
+
+
+SWEP.ClipSize = 30
+SWEP.Ammo = "smg1"
+
+SWEP.ReloadTimeMult = 1
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/ak47.mdl"
+SWEP.DropMagazineImpact = "metal"
+
+SWEP.ReloadUpInTime = 1.65
+SWEP.DropMagazineTime = 0.65
+
+SWEP.FreeAimMaxAngle = 4.5
+
+// sounds
+
+local path = "tacrp/weapons/ak47/ak47_"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 130
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 2
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "deploy",
+    ["fire_iron"] = "fire1_M",
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"},
+    ["jam"] = "mid_reload"
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["foldstock"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+        BGs_WM = {
+            {1, 1}
+        }
+    },
+    ["tactical"] = {
+        BGs_VM = {
+            {2, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = {"optic_cqb", "optic_medium", "optic_sniper"},
+        Bone = "ValveBiped.AK47_rootbone",
+        WMBone = "Box01",
+        InstalledElements = {"tactical"},
+        AttachSound = "tacrp/weapons/optic_on.wav",
+        DetachSound = "tacrp/weapons/optic_off.wav",
+        VMScale = 0.75,
+        Pos_VM = Vector(-5.15, 0.15, 4),
+        Ang_VM = Angle(90, 0, 0),
+        Pos_WM = Vector(0, 3, 0.5),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped.AK47_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacrp/weapons/silencer_on.wav",
+        DetachSound = "tacrp/weapons/silencer_off.wav",
+        Pos_VM = Vector(-3.2, 0.075, 27.6),
+        Pos_WM = Vector(0, 28, -1.75),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [3] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.AK47_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+        Pos_VM = Vector(-3.25, -0.1, 19),
+        Pos_WM = Vector(0, 19, -2),
+        Ang_VM = Angle(90, 0, -90),
+        Ang_WM = Angle(0, -90, 180),
+    },
+    [4] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_foldstock", "acc_sling", "acc_duffle", "perk_extendedmag"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+    [5] = {
+        PrintName = "Bolt",
+        Category = {"bolt_automatic"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [6] = {
+        PrintName = "Trigger",
+        Category = {"trigger_auto"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [7] = {
+        PrintName = "Ammo",
+        Category = {"ammo_rifle"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [8] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_ak47.remove_clip", path .. "remove_clip.wav")
+addsound("tacint_ak47.insert_clip", path .. "insert_clip.wav")
+addsound("tacint_ak47.boltaction", path .. "boltaction.wav")
+addsound("tacint_ak47.Buttstock_Back", path .. "buttstock_back.wav")
