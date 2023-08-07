b72124d5a941d381e15e80a850d84987d5ce1ff4
Fesiug <publicfesiug@outlook.com>
2023-08-07 14:42:46 -0400

    Make FAMAS not make me want to VOMIT!!

--- lua/weapons/tacrp_sd_famas.lua
+++ lua/weapons/tacrp_sd_famas.lua
@@ -111,7 +111,7 @@ SWEP.RecoilResetTime = 0.2
 SWEP.RecoilDissipationRate = 15
 SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
 
-SWEP.RecoilVisualKick = 2
+SWEP.RecoilVisualKick = 1
 SWEP.RecoilKick = 6
 SWEP.RecoilStability = 0.4
 
