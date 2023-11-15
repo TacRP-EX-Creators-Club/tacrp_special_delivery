9eb7d06603156b3fe9635498677d6052f7108bd8
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-11-15 14:47:52 -0500

    actually nevermind
    looked bad

--- lua/weapons/tacrp_sd_mp40.lua
+++ lua/weapons/tacrp_sd_mp40.lua
@@ -212,8 +212,6 @@ SWEP.AnimationTranslationTable = {
     ["melee"] = {"melee1", "melee2"}
 }
 
-SWEP.LastShot = true
-
 SWEP.ProceduralIronFire = {
     vm_pos = Vector(0, -0.3, -0.15),
     vm_ang = Angle(0, 0.2, 0),
