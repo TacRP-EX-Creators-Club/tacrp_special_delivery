59103305b0d8bf7181cccd074a6654fdd7670d09
TheOnly8Z <888888zombies@gmail.com>
2024-01-21 18:50:43 -0800

    why does m1 carbine use ar2 ammo

--- lua/weapons/tacrp_sd_m1carbine.lua
+++ lua/weapons/tacrp_sd_m1carbine.lua
@@ -164,7 +164,7 @@ SWEP.HolsterAng = Angle(0, 0, 0)
 // reload
 
 SWEP.ClipSize = 15
-SWEP.Ammo = "ar2"
+SWEP.Ammo = "smg1"
 
 SWEP.ReloadTimeMult = 0.85
 SWEP.DropMagazineModel = "models/weapons/tacint_shark/magazines/m1carbine.mdl"
