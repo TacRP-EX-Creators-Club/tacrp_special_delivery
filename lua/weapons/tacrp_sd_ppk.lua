678f6a5cb4677af96a313aed74b97a8f731b63fd
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-15 21:12:18 -0400

    garbajgh

--- lua/weapons/tacrp_sd_ppk.lua
+++ lua/weapons/tacrp_sd_ppk.lua
@@ -301,7 +301,7 @@ SWEP.Attachments = {
     },
     [2] = {
         PrintName = "Tactical",
-        Category = {"tactical_zoom", "tactical_ebullet"},
+        Category = {"tactical_ebullet"},
         Bone = "sphinx_ROOT",
         WMBone = "Box01",
         AttachSound = "TacRP/weapons/flashlight_on.wav",
