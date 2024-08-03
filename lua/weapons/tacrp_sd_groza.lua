713e5b59c2da33aa66f325055407f7f1e60c91ea
TheOnly8Z <888888zombies@gmail.com>
2024-08-03 14:44:55 +0800

    too loud mate

--- lua/weapons/tacrp_sd_groza.lua
+++ lua/weapons/tacrp_sd_groza.lua
@@ -180,7 +180,7 @@ local path1 = "tacrp/weapons/ak47/ak47_"
 SWEP.Sound_Shoot = "^" .. path .. "groza_fire-1.wav"
 SWEP.Sound_Shoot_Silenced = "^" .. path .. "groza_fire-1.wav"
 
-SWEP.Vol_Shoot = 110
+SWEP.Vol_Shoot = 75
 SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
 
 // effects
